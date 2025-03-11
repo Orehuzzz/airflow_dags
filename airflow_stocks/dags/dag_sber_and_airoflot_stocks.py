6233bf5cbe3821cd14bcf7dfc23ca4453cb7fc3c
Orehuzzz <oreshin.vitalik@gmail.com>
2025-03-11 22:01:56 +0300

    Add files

---
+++ airflow_stocks/dags/dag_sber_and_airoflot_stocks.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.hooks.base_hook import BaseHook
+connection = BaseHook.get_connection('main_postgresql_connection')
+
+
+default_args = {
+    "owner": "etl_user",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 3, 11),
+    #"retry_delay": timedelta(minutes=0.1)
+}
+
+dag = DAG('sber_stock_dag', default_args=default_args, schedule_interval='55 * * * *', catchup=True,
+          max_active_tasks=3, max_active_runs=1, tags=["Stocks dag", "test stocks dag"])
+
+task1 = BashOperator(
+    task_id='task_stock_sber',
+    bash_command='python3 /root/airflow/scripts/task_stock_sber.py --date {{ ds }} ' +f'--host {connection.host} --dbname {connection.schema} --user {connection.login} --jdbc_password {connection.password} --port 5432',
+    dag=dag)
+
+task2 = BashOperator(
+    task_id='task_stock_airoflot',
+    bash_command='python3 /root/airflow/scripts/task_stock_airoflot.py --date {{ ds }} ' +f'--host {connection.host} --dbname {connection.schema} --user {connection.login} --jdbc_password {connection.password} --port 5432',
+    dag=dag)
+
---
+++ airflow_stocks/scripts/task_stock_sber.py
@@ -0,0 +1,116 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, VARCHAR, Date, Boolean, Float, TIMESTAMP, text, TIME
+from sqlalchemy.orm import declarative_base
+import argparse
+import requests
+import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--date", dest="date")
+parser.add_argument("--host", dest="host")
+parser.add_argument("--dbname", dest="dbname")
+parser.add_argument("--user", dest="user")
+parser.add_argument("--jdbc_password", dest="jdbc_password")
+parser.add_argument("--port", dest="port")
+args = parser.parse_args()
+
+print('date = ' + str(args.date))
+print('host = ' + str(args.host))
+print('dbname = ' + str(args.dbname))
+print('user = ' + str(args.user))
+print('jdbc_password = ' + str(args.jdbc_password))
+print('port = ' + str(args.port))
+
+v_host = str(args.host)
+v_dbname = str(args.dbname)
+v_user = str(args.user)
+v_password = str(args.jdbc_password)
+v_port = str(args.port)
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{v_user}:{v_password}@{v_host}:{v_port}/{v_dbname}"
+
+Base = declarative_base()
+
+
+class StocksSber(Base):
+    __tablename__ = 'sber_stocks'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    time = Column(TIME)
+    date = Column(Date)
+    open_price = Column(Float)
+    low_price = Column(Float)
+    high_price = Column(Float)
+    last_price = Column(Float)
+    usd_price = Column(Float)
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+class RequestSender:
+    def __init__(self):
+        self.data_json = None
+
+    def get_SBER_stocks(self):
+        self.URL = f'https://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER.json'
+        try:
+            response = requests.get(url=self.URL)
+            response.raise_for_status()
+            self.data_json = response.json()
+        except:
+            print('API request Error')
+
+
+
+stocks = RequestSender()
+stocks.get_SBER_stocks()
+
+columns = stocks.__dict__['data_json']['marketdata']['columns']
+values = stocks.__dict__['data_json']['marketdata']['data'][0]
+
+
+last_price_index = columns.index("LAST")
+open_price_index = columns.index('OPEN')
+usd_price_index = columns.index('VALUE_USD')
+time_index = columns.index('TIME')
+low_price_index = columns.index('LOW')
+high_price_index = columns.index('HIGH')
+
+
+last_price = values[last_price_index]
+open_price = values[open_price_index]
+usd_price = values[usd_price_index]
+time = values[time_index]
+date = datetime.date.today().isoformat()
+low_price = values[low_price_index]
+high_price = values[high_price_index]
+
+
+Base.metadata.create_all(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session_local = SessionLocal()
+
+new_record = StocksSber(
+                    time=time,
+                    date=date,
+                    open_price=open_price,
+                    low_price=low_price,
+                    high_price=high_price,
+                    last_price=last_price,
+                    usd_price=usd_price
+    )
+# new_record_two = Weather(
+#                     temp=temp,
+#                     feels_like=feels_like,
+#                     temp_min=temp_min,
+#                     pressure=pressure,
+#                     humidity=humidity,
+#                     sea_level=sea_level,
+#                     grnd_level=grnd_level
+#                     )
+#При добавлении второго + параметра
+
+
+session_local.add(new_record)
+session_local.commit()
