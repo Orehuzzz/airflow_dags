47ed8cdd4cc72673395277f616e7cdaf7897b873
Orehuzzz <oreshin.vitalik@gmail.com>
2025-05-03 22:04:36 +0300

    tasks and dags

--- dags/dag_behavior_dataset.py
+++ dags/dag_behavior_dataset.py
@@ -1,55 +1,17 @@
 #Незаконченная попытка в спарк
 from datetime import datetime
-import argparse
-from sqlalchemy import create_engine
+from params.load_csv_to_db import load_csv_to_db
 from airflow.models import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-import pandas as pd
+from airflow.hooks.base_hook import BaseHook
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--date", dest="date")
-parser.add_argument("--host", dest="host")
-parser.add_argument("--dbname", dest="dbname")
-parser.add_argument("--user", dest="user")
-parser.add_argument("--jdbc_password", dest="jdbc_password")
-parser.add_argument("--port", dest="port")
-args = parser.parse_args()
-
-print('date = ' + str(args.date))
-print('host = ' + str(args.host))
-print('dbname = ' + str(args.dbname))
-print('user = ' + str(args.user))
-print('jdbc_password = ' + str(args.jdbc_password))
-print('port = ' + str(args.port))
-
-v_host = str(args.host)
-v_dbname = str(args.dbname)
-v_user = str(args.user)
-v_password = str(args.jdbc_password)
-v_port = str(args.port)
-
-SQLALCHEMY_DATABASE_URI = f"postgresql://{str(v_user)}:{str(v_password)}@{str(v_host)}:{str(v_port)}/{str(v_dbname)}"
-
-def load_csv_to_db():
-    # Загрузка CSV в DataFrame
-    df = pd.read_csv('/jupyter_notebook_files/data/clear/part-00000-ea3365ac-0859-425a-a100-115b517ee248-c000.csv')
-
-    # Создание подключения к базе данных (PostgreSQL в примере)
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
-
-
-    df.to_sql('', engine, index=False,
-              if_exists='replace')
-
-
-# Определение DAG
+connection = BaseHook.get_connection('main_postgresql_connection')
 
 default_args = {
     "owner": "etl_user",
     "depends_on_past": False, #узнать что значит параметр
-    "start_date": datetime(2025, 2, 18),
+    "start_date": datetime(2025, 5, 1)
     #"retry_delay": timedelta(minutes=0.1)
 }
 
